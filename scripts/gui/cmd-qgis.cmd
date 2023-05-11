f1c85832d4be7aa190cfe83121cd52888d55a4e5
Brian Miles <selimnairb@gmail.com>
2023-05-11 16:28:40 -0400

    Debug cmd-qgis.cmd

--- scripts/gui/cmd-qgis.cmd
+++ scripts/gui/cmd-qgis.cmd
@@ -2,16 +2,16 @@
 set OSGEO4W_ROOT=C:\OSGeo4W
 call "%OSGEO4W_ROOT%"\bin\o4w_env.bat
 @echo off
-path %PATH%;%OSGEO4W_ROOT%\apps\qgis\bin
+path %PATH%;%OSGEO4W_ROOT%\apps\qgis-ltr\bin
 path %PATH%;%OSGEO4W_ROOT%\apps\Qt5\bin
 path %PATH%;%OSGEO4W_ROOT%\apps\Python39\Scripts
-set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis
+set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis-ltr
 set GDAL_FILENAME_IS_UTF8=YES
 rem Set VSI cache to be used as buffer, see #6448
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
-set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\qgis\python
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis-ltr\python;.\
 set PYTHONHOME=%OSGEO4W_ROOT%\apps\Python39
-set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
+set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis-ltr\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
 
 cmd %*
